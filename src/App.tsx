37fc00161187246cc1c534116ae8beced8ec8316
zestzero <gmkipladen@gmail.com>
2021-02-20 17:14:38 +0700

    Trying deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 
 function App() {
   return (
-    <div>
-      <style jsx>{`
-        .App {
-          text-align: center;
-        }
-
-        .App-logo {
-          height: 40vmin;
-          pointer-events: none;
-        }
-
-        @media (prefers-reduced-motion: no-preference) {
-          .App-logo {
-            animation: App-logo-spin infinite 20s linear;
-          }
-        }
-
-        .App-header {
-          background-color: #282c34;
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          font-size: calc(10px + 2vmin);
-          color: white;
-        }
-
-        .App-link {
-          color: #61dafb;
-        }
-
-        @keyframes App-logo-spin {
-          from {
-            transform: rotate(0deg);
-          }
-          to {
-            transform: rotate(360deg);
-          }
-        }
-      `}</style>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <h1>NEXTJS FIREBASE KRITB</h1>
+      </header>
     </div>
   );
 }
